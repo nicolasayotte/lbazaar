3761bf889ab2be80ff0957022bccc6d54fff93c5
Phillip Quipse <p.quipse@bizwind.co.jp>
2022-11-07 11:28:46 +0800

    Create tentative base layout

--- resources/js/layouts/Layout.jsx
+++ resources/js/layouts/Layout.jsx
@@ -1,10 +1,34 @@
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
+import { useState } from "react";
+import { useEffect } from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 const Layout = ({ children }) => {
+
+    const [containerHeight, setContainerHeight] = useState(0);
+
+    useEffect(() => {
+        let screenHeight = window.innerHeight
+
+        var navbarHeight = document.getElementById('navbar').clientHeight
+        var footerHeight = document.getElementById('footer').clientHeight
+
+        setContainerHeight(screenHeight - (navbarHeight + footerHeight))
+
+        console.log(screenHeight, navbarHeight, footerHeight, containerHeight);
+    }, [])
+
     return (
-        <Container>
-            { children }
-        </Container>
+        <>
+            <Navbar />
+            <Box sx={{ minHeight: containerHeight + 'px' }}>
+                <Container>
+                    { children }
+                </Container>
+            </Box>
+            <Footer />
+        </>
     )
 }
 
