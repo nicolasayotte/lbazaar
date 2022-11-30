fd402bb5eacb1d861a3400700c17209b131b270b
Phillip Quipse <p.quipse@bizwind.co.jp>
2022-12-01 07:51:18 +0800

    Remove navbar and footer

--- resources/js/layouts/Layout.jsx
+++ resources/js/layouts/Layout.jsx
@@ -1,33 +1,14 @@
 import { Box, Container } from "@mui/material";
-import { useState } from "react";
-import { useEffect } from "react";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
 
 const Layout = ({ children }) => {
 
-    const [containerHeight, setContainerHeight] = useState(0);
-
-    useEffect(() => {
-        let screenHeight = window.innerHeight
-
-        var navbarHeight = document.getElementById('navbar').clientHeight
-        var footerHeight = document.getElementById('footer').clientHeight
-
-        setContainerHeight(screenHeight - (navbarHeight + footerHeight))
-
-        console.log(screenHeight, navbarHeight, footerHeight, containerHeight);
-    }, [])
-
     return (
         <>
-            <Navbar />
-            <Box sx={{ minHeight: containerHeight + 'px' }}>
+            <Box>
                 <Container>
                     { children }
                 </Container>
             </Box>
-            <Footer />
         </>
     )
 }
