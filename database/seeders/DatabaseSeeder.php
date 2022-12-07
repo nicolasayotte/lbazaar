093b5b0570f82d88433147d8084ffea60b4d99a8
Phillip Quipse <p.quipse@bizwind.co.jp>
2022-12-07 15:44:05 +0800

    #225 Create CourseCategory class seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ClassificationSeeder::class,
             StatusSeeder::class,
-            CourseTypeSeeder::class
+            CourseTypeSeeder::class,
+            CourseCategorySeeder::class
         ]);
     }
 }
