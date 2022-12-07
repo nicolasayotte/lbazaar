20171353f7825111d98842d5c438f282785c1471
Phillip Quipse <p.quipse@bizwind.co.jp>
2022-12-07 12:28:19 +0800

    #225 Create Status class seeder

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Status extends Model
 {
     use HasFactory;
+
+    public const PENDING  = 'pending';
+    public const APPROVED = 'approved';
+    public const DENIED   = 'denied';
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            ClassificationSeeder::class
+            ClassificationSeeder::class,
+            StatusSeeder::class
         ]);
     }
 }
