9531e2cf0160aa754439a1625d5ee7f92327108f
Phillip Quipse <p.quipse@bizwind.co.jp>
2022-12-13 14:58:17 +0800

    close #22 - Initial inquiries page layout

--- resources/js/Pages/portal/Inquiries.jsx
+++ resources/js/Pages/portal/Inquiries.jsx
@@ -1,6 +1,56 @@
+import { Box, Button, Card, CardContent, Container, Divider, Grid, TextField, Typography } from "@mui/material"
+
 const Inquiries = () => {
+
     return (
-        <><h1>Inquiries</h1></>
+        <Box my={5}>
+            <Container>
+                <Grid container>
+                    <Grid item xs={12} sm={8} mx="auto" py={5}>
+                        <Card>
+                            <CardContent sx={{ p: 3 }}>
+                                <form>
+                                    <Typography variant="h5">Inquiry</Typography>
+                                    <Divider sx={{ my: 2 }} />
+                                    <Grid container spacing={2}>
+                                        <Grid item xs={12} sm={6}>
+                                            <TextField
+                                                label="Name"
+                                                fullWidth
+                                            />
+                                        </Grid>
+                                        <Grid item xs={12} sm={6} >
+                                            <TextField
+                                                label="Email Address"
+                                                type="email"
+                                                fullWidth
+                                            />
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <TextField
+                                                label="Subject"
+                                                fullWidth
+                                            />
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <TextField
+                                                label="Message"
+                                                multiline
+                                                fullWidth
+                                                minRows={9}
+                                            />
+                                        </Grid>
+                                        <Grid item xs={12} textAlign="right">
+                                            <Button variant="contained">Submit</Button>
+                                        </Grid>
+                                    </Grid>
+                                </form>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
     )
 }
 
