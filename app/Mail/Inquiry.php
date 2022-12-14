f908bc3c4e654c1a5440dfa65edc0a8d847d4c27
Phillip Quipse <p.quipse@bizwind.co.jp>
2022-12-14 13:22:06 +0800

    Create mailable class for inquiries

---
+++ app/Mail/Inquiry.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Inquiry as InquiryModel;
+use App\Repositories\SettingRepository;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class Inquiry extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * The inquiry instance
+     *
+     * @var \App\Models\Inquiry
+     */
+    public $inquiry;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(InquiryModel $inquiry)
+    {
+        $this->inquiry = $inquiry;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        $settingRepository = new SettingRepository();
+
+        return new Envelope(
+            subject: $this->inquiry->subject,
+            from: new Address($this->inquiry->email, $this->inquiry->name),
+            to: [$settingRepository->getSetting('inquiry-receiver-email')]
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'emails.inquiry',
+            with: [
+                'inquiry' => $this->inquiry
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
---
+++ resources/views/emails/inquiry.blade.php
@@ -0,0 +1,5 @@
+@extends('emails.layout')
+
+@section('content')
+    <p>{{ @$inquiry->message }}</p>
+@endsection
