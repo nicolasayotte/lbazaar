cac960d83f6e6eab334393aacb35dccff29a208c
Phillip Quipse <p.quipse@bizwind.co.jp>
2022-12-16 16:46:40 +0800

    close #210 - Layout admin template

---
+++ resources/js/components/includes/AdminNavbar.jsx
@@ -0,0 +1,125 @@
+import { Link } from "@inertiajs/inertia-react"
+import { Menu } from "@mui/icons-material"
+import { AppBar, Box, Button, Divider, Drawer, Grid, IconButton, List, ListItem, ListItemButton, Toolbar, Typography } from "@mui/material"
+import { useState } from "react"
+
+const AdminNavbar = ({ drawerWidth, setDrawerWidth, window }) => {
+
+    const container = window !== undefined ? () => window().document.body : undefined;
+
+    const [openMobileDrawer, setopenMobileDrawer] = useState(false)
+
+    const toggleMobileDrawer = () => {
+        setopenMobileDrawer(!openMobileDrawer)
+    }
+
+    const navItems = [
+        'Class Applications',
+        'Class List',
+        'Manage Users',
+        'Inquiries',
+        'Settings'
+    ]
+
+    const menu = (
+        <>
+            {navItems.map(item => (
+                <ListItem key={item}>
+                    <ListItemButton>{item}</ListItemButton>
+                </ListItem>
+            ))}
+        </>
+    )
+
+    const desktopDrawer = (
+        <Drawer
+            variant="permanent"
+            anchor="left"
+            container={container}
+            open
+            sx={{
+                display: { xs: 'none', md: 'block' },
+                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+            }}
+        >
+            <Toolbar>
+                <Typography variant="h6" textAlign="center">ADMIN</Typography>
+            </Toolbar>
+            <Divider />
+            <List>
+                {menu}
+            </List>
+        </Drawer>
+    )
+
+    const mobileDrawer = (
+        <Drawer
+            variant="temporary"
+            anchor="left"
+            container={container}
+            open={openMobileDrawer}
+            onClose={toggleMobileDrawer}
+            ModalProps={{
+                keepMounted: true
+            }}
+            sx={{
+                display: { xs: 'block', md: 'none' },
+                '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
+        >
+            <Toolbar>
+                <Typography variant="h6" textAlign="center">ADMIN</Typography>
+            </Toolbar>
+            <Divider />
+            <List>
+                {menu}
+                <Divider sx={{ my: 1 }} />
+                <ListItem key="profile">
+                    <ListItemButton>Profile</ListItemButton>
+                </ListItem>
+                <ListItem key="sign out">
+                    <ListItemButton>Sign Out</ListItemButton>
+                </ListItem>
+            </List>
+        </Drawer>
+    )
+
+    return (
+        <>
+            <AppBar
+                position="static"
+                color="primary"
+                sx={{ zIndex: theme => theme.zIndex.drawer + 1 }}
+            >
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        textAlign="center"
+                        sx={{ display: { xs: 'inline-block', md: 'none' } }}
+                    >ADMIN</Typography>
+                    <Box sx={{ ml: 'auto' }}>
+                        <Grid container sx={{ display: { xs: 'none', md: 'flex' } }}>
+                            <Grid item>
+                                <ListItemButton>Profile</ListItemButton>
+                            </Grid>
+                            <Grid item>
+                                <ListItemButton>Sign Out</ListItemButton>
+                            </Grid>
+                        </Grid>
+                        <IconButton
+                            color="white"
+                            sx={{ display: { xs: 'inline-block', md: 'none' } }}
+                            onClick={toggleMobileDrawer}
+                        >
+                            <Menu color="inherit" />
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            {desktopDrawer}
+            {mobileDrawer}
+        </>
+    )
+}
+
+export default AdminNavbar
--- resources/js/layouts/Admin.jsx
+++ resources/js/layouts/Admin.jsx
@@ -1,8 +1,23 @@
+import { Container } from "@mui/material"
+import { Box } from "@mui/system"
+import { useState } from "react"
+import AdminNavbar from "../components/includes/AdminNavbar"
 
 const Admin = ({children}) => {
+
+    const [drawerWidth, setDrawerWidth] = useState(300)
+
     return (
         <>
-            {children}
+            <AdminNavbar
+                drawerWidth={drawerWidth}
+                setDrawerWidth={setDrawerWidth}
+            />
+            <Box sx={{ paddingLeft: { xs: 0, md: `${ drawerWidth }px` }}}>
+                <Container>
+                    {children}
+                </Container>
+            </Box>
         </>
     )
 }
