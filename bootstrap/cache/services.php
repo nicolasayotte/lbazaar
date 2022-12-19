d79112b67fc734150366c21f8c5d23ebc73b60f0
Phillip Quipse <p.quipse@bizwind.co.jp>
2022-12-19 12:42:40 +0800

    Install country-list package

--- bootstrap/cache/services.php
+++ bootstrap/cache/services.php
@@ -32,10 +32,11 @@
     28 => 'Termwind\\Laravel\\TermwindServiceProvider',
     29 => 'Laratrust\\LaratrustServiceProvider',
     30 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
-    31 => 'App\\Providers\\AppServiceProvider',
-    32 => 'App\\Providers\\AuthServiceProvider',
-    33 => 'App\\Providers\\EventServiceProvider',
-    34 => 'App\\Providers\\RouteServiceProvider',
+    31 => 'Monarobase\\CountryList\\CountryListServiceProvider',
+    32 => 'App\\Providers\\AppServiceProvider',
+    33 => 'App\\Providers\\AuthServiceProvider',
+    34 => 'App\\Providers\\EventServiceProvider',
+    35 => 'App\\Providers\\RouteServiceProvider',
   ),
   'eager' => 
   array (
@@ -56,10 +57,11 @@
     14 => 'Termwind\\Laravel\\TermwindServiceProvider',
     15 => 'Laratrust\\LaratrustServiceProvider',
     16 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
-    17 => 'App\\Providers\\AppServiceProvider',
-    18 => 'App\\Providers\\AuthServiceProvider',
-    19 => 'App\\Providers\\EventServiceProvider',
-    20 => 'App\\Providers\\RouteServiceProvider',
+    17 => 'Monarobase\\CountryList\\CountryListServiceProvider',
+    18 => 'App\\Providers\\AppServiceProvider',
+    19 => 'App\\Providers\\AuthServiceProvider',
+    20 => 'App\\Providers\\EventServiceProvider',
+    21 => 'App\\Providers\\RouteServiceProvider',
   ),
   'deferred' => 
   array (
