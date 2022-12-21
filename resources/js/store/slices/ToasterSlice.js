ce6dc2b09910d6540ce81da8e084c97000d28ced
Phillip Quipse <p.quipse@bizwind.co.jp>
2022-12-21 16:06:48 +0800

    Fix alert toaster close button

--- resources/js/store/slices/ToasterSlice.js
+++ resources/js/store/slices/ToasterSlice.js
@@ -1,27 +1,37 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const types = {
+    success: 'success',
+    error: 'error'
+}
+
+const initialState = {
+    open: false,
+    message: '',
+    type: types.success,
+    duration: 3000
+}
+
+
 const ToasterSlice = createSlice({
     name: 'toaster',
-    initialState: {
-        open: false,
-        message: '',
-        type: 'success'
-    },
+    initialState: initialState,
     reducers: {
-        toggle: (state, action) => {
-            state.open = action.payload.open
-            state.type = action.payload.type ?? state.type
-            state.message = action.payload.message ?? state.message
+        success: (state, action) => state = {
+            ...state,
+            open: true,
+            type: types.success,
+            ...action.payload
         },
-        success: (state, action) => {
-            state.open = true
-            state.type = 'success'
-            state.message = action.payload.message ?? state.message
+        error: (state, action) => state = {
+            ...state,
+            open: true,
+            type: types.error,
+            ...action.payload
         },
-        error: (state, action) => {
-            state.open = true
-            state.type = 'error'
-            state.message = action.payload.message ?? state.message
+        hide: (state, action) => state = {
+            ...state,
+            open: false
         }
     }
 })
