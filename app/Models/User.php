4520187d4b860278b20dcb0813af2d6a0039ff52
Allysa Noceda <a.noceda@bizwind.co.jp>
2022-12-31 23:38:17 +0800

    Added email verification of user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,17 +2,18 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laratrust\Traits\LaratrustUserTrait;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+
+class User extends Authenticatable implements MustVerifyEmail
 {
     use LaratrustUserTrait;
-    use HasApiTokens, HasFactory, Notifiable;
+    use  HasApiTokens, HasFactory, Notifiable;
 
     const FEATURED_TEACHERS_COUNT_DISPLAY = 3;
 
@@ -27,8 +28,8 @@ class User extends Authenticatable
         'first_name',
         'last_name',
         'email',
+        'country_id',
         'password',
-        'country_id'
     ];
 
     /**
@@ -55,4 +56,8 @@ class User extends Authenticatable
         return $this->first_name . ' ' . $this->last_name;
     }
 
+    public function getEmailVerifiedAt()
+    {
+        return $this->email_verified_at;
+    }
 }
