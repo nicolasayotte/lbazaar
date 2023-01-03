f7a9e7bd164fe53c3f6759a021400dadf9db66d1
Phillip Quipse <p.quipse@bizwind.co.jp>
2023-01-03 14:50:04 +0800

    close #70 - Layout user details page

--- resources/js/pages/Admin/Users/UserView.jsx
+++ resources/js/pages/Admin/Users/UserView.jsx
@@ -1,6 +1,52 @@
+import { Link } from "@inertiajs/inertia-react"
+import { Box, Breadcrumbs, Paper, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material"
+import routes from "../../../helpers/routes.helper"
+
 const UserView = () => {
     return (
-        <>User View</>
+        <Box>
+            <Typography
+                variant="h4"
+                children="User"
+                gutterBottom
+            />
+            <Box sx={{ mb: 2 }}>
+                <Breadcrumbs>
+                    <Link href={routes["admin.users.index"]} children="Manage Users" />
+                    <Typography color={'text.primary'} children="User" />
+                </Breadcrumbs>
+            </Box>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell children="Name" />
+                            <TableCell align="right" children="John Smith" />
+                        </TableRow>
+                        <TableRow>
+                            <TableCell children="Email" />
+                            <TableCell align="right" children="johnsmith@example.com" />
+                        </TableRow>
+                        <TableRow>
+                            <TableCell children="Roles" />
+                            <TableCell align="right" children="Student / Teacher" />
+                        </TableRow>
+                        <TableRow>
+                            <TableCell children="Country" />
+                            <TableCell align="right" children="United States" />
+                        </TableRow>
+                        <TableRow>
+                            <TableCell children="Date Joined" />
+                            <TableCell align="right" children="John Smith" />
+                        </TableRow>
+                        <TableRow>
+                            <TableCell children="Status" />
+                            <TableCell align="right" children="Active" />
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Box>
     )
 }
 
