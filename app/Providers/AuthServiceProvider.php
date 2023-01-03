2fd8efd9471cfc1c05dfb97bdf2bd0f9645e7e1a
Allysa Noceda <a.noceda@bizwind.co.jp>
2023-01-03 16:43:26 +0800

    Moved to EmailNotification file

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Providers;
+
+use App\Mail\EmailNotification;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Notifications\Messages\MailMessage;
 
@@ -28,10 +30,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
-            return (new MailMessage)
-                ->subject('Verify Email Address')
-                ->line('Click the button below to verify your email address.')
-                ->action('Verify Email Address', $url);
+            return (new EmailNotification($notifiable, $url));
         });
     }
 }
