f7bfd3abc1bb4283b2888caf986daa0c03263c7b
Phillip Quipse <p.quipse@bizwind.co.jp>
2023-01-05 16:14:46 +0800

    Create ConfirmationDialog component

---
+++ resources/js/components/common/ConfirmationDialog.jsx
@@ -0,0 +1,30 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material"
+
+const ConfirmationDialog = ({ open, title, text, handleClose, handleConfirm, cancelButtonText = 'Cancel', confirmButtonText = 'Confirm' }) => {
+    return (
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            fullWidth
+            maxWidth="xs"
+        >
+            <DialogTitle children={title} sx={{ p: 2 }} />
+            <DialogContent dividers sx={{ p: 2 }}>
+                <DialogContentText children={text} />
+            </DialogContent>
+            <DialogActions sx={{ p: 2 }}>
+                <Button
+                    children={cancelButtonText}
+                    onClick={handleClose}
+                />
+                <Button
+                    variant="contained"
+                    children={confirmButtonText}
+                    onClick={handleConfirm}
+                />
+            </DialogActions>
+        </Dialog>
+    )
+}
+
+export default ConfirmationDialog
