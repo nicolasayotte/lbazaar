2bc6034b913bd5d51f84abc9bccc3c7be3bc04c5
Jerome Del Rosario <j.delrosario@bizwind.co.jp>
2023-01-09 08:59:18 +0800

    Add Filter form layout in class history

--- app/Http/Controllers/Portal/ClassHistoryController.php
+++ app/Http/Controllers/Portal/ClassHistoryController.php
@@ -4,25 +4,49 @@ namespace App\Http\Controllers\Portal;
 
 use App\Http\Controllers\Controller;
 use App\Models\Country;
+use App\Repositories\CourseCategoryRepository;
+use App\Repositories\CourseContentRepository;
+use App\Repositories\CourseRepository;
+use App\Repositories\CourseTypeRepository;
 use App\Repositories\UserRepository;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class ClassHistoryController extends Controller
 {
-    private $userRepository;
+    public $courseTypeRepository;
+
+    public $courseCategoryRepository;
+
+    public $courseRepository;
+
+    public $courseContentRepository;
+    
+    public $userRepository;
 
     public function __construct()
     {
+        $this->courseTypeRepository = new CourseTypeRepository();
+        $this->courseCategoryRepository = new CourseCategoryRepository();
+        $this->courseRepository = new CourseRepository();
+        $this->courseContentRepository = new CourseContentRepository();
         $this->userRepository = new UserRepository();
     }
 
     public function index()
     {
         $countries = Country::all();
+        $languages = $this->courseRepository->getLanguages();
+        $types = $this->courseTypeRepository->getAll();
+        $categories = $this->courseCategoryRepository->getAll();
+        $teachers = $this->userRepository->getAllTeachers();
         
         return Inertia::render('Portal/MyPage/ClassHistory/Index', [
-            'countries' => $countries
+            'course_types'          => $types,
+            'course_categories'     => $categories,
+            'languages'             => $languages,
+            'teachers'              => $teachers,
+            'countries'             => $countries
         ])->withViewData([
             'title' => 'Class Histories | My page'
         ]);
