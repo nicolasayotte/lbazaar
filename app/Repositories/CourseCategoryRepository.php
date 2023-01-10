0890e5b4792d3850627206331492bdb44eae3542
Phillip Quipse <p.quipse@bizwind.co.jp>
2023-01-10 15:22:49 +0800

    Define getDropdownData() methods used for filtering course applications

--- app/Repositories/CourseCategoryRepository.php
+++ app/Repositories/CourseCategoryRepository.php
@@ -3,12 +3,21 @@
 namespace App\Repositories;
 
 use App\Models\CourseCategory;
-use Carbon\Carbon;
 
 class CourseCategoryRepository extends BaseRepository
 {
     public function __construct()
     {
         parent::__construct(new CourseCategory());
+    }
+
+    public function getDropdownData()
+    {
+        return $this->model->all()->map(function($data) {
+            return [
+                'id'   => $data->id,
+                'name' => $data->name
+            ];
+        });
     }
 }
