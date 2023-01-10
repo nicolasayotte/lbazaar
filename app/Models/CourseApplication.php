81d808c6628fe211314176abb18a0776573788f7
Phillip Quipse <p.quipse@bizwind.co.jp>
2023-01-10 15:21:44 +0800

    Define constants and relationship methods for CourseApplication model

--- app/Models/CourseApplication.php
+++ app/Models/CourseApplication.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class CourseApplication extends Model
 {
     use HasFactory;
+
+    public const PENDING = 'pending';
+
+    public const APPROVED = 'approved';
+
+    public const DENIED = 'denied';
+
+    public const PER_PAGE = 10;
+
+    public function professor()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function courseType()
+    {
+        return $this->belongsTo(CourseType::class);
+    }
+
+    public function courseCategory()
+    {
+        return $this->belongsTo(CourseCategory::class);
+    }
 }
