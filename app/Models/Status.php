65bcb005291b0adbaecbf26973d1764b63739962
Jerome Del Rosario <j.delrosario@bizwind.co.jp>
2023-01-13 07:56:40 +0800

    Remove getStatusById

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -12,16 +12,4 @@ class Status extends Model
     public const PENDING  = 'pending';
     public const APPROVED = 'approved';
     public const DENIED   = 'denied';
-
-    public static function getStatusNameByID($id)
-    {
-        switch ($id) {
-            case 1:
-                return ucfirst(self::PENDING);
-            case 2:
-                return ucfirst(self::APPROVED); 
-            default:
-                return ucfirst(self::DENIED);
-        }
-    }
 }
