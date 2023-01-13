7d6a8811fe2d1f1a985294bdb780cf01f424713a
Phillip Quipse <p.quipse@bizwind.co.jp>
2023-01-13 16:05:18 +0800

    Added new properties to CourseApplicationData

--- app/Data/CourseApplicationData.php
+++ app/Data/CourseApplicationData.php
@@ -2,7 +2,6 @@
 
 namespace App\Data;
 
-use App\Models\Course;
 use App\Models\CourseApplication;
 use Carbon\Carbon;
 
@@ -10,117 +9,213 @@ class CourseApplicationData
 {
     private $id;
 
-    private $title;
+	private $title;
 
-    private $professor;
+	private $professor_name;
 
-    private $type;
+	private $professor_email;
 
-    private $category;
+	private $professor_classification;
 
-    private $price;
+	private $professor_created_at;
 
-    private $created_at;
+	private $type;
 
-    private $status;
+	private $price;
+
+	private $category;
+
+	private $created_at;
+
+	private $status;
+
+	private $description;
+
+	private $points_earned;
+
+	private $approved_at;
+
+	private $denied_at;
+
+	private $language;
 
     // Setters
     public function setId($id)
-    {
-        $this->id = $id;
+	{
+		$this->id = $id;
+		return $this;
+	}
 
-        return $this;
-    }
+	public function setTitle($title)
+	{
+		$this->title = $title;
+		return $this;
+	}
 
-    public function setTitle($title)
-    {
-        $this->title = $title;
+	public function setProfessorName($professor_name)
+	{
+		$this->professor_name = $professor_name;
+		return $this;
+	}
 
-        return $this;
-    }
+	public function setProfessorEmail($professor_email)
+	{
+		$this->professor_email = $professor_email;
+		return $this;
+	}
 
-    public function setProfessor($professor)
-    {
-        $this->professor = $professor;
+	public function setProfessorClassification($professor_classification)
+	{
+		$this->professor_classification = $professor_classification;
+		return $this;
+	}
 
-        return $this;
-    }
+	public function setProfessorCreatedAt($professor_created_at)
+	{
+		$this->professor_created_at = $professor_created_at;
+		return $this;
+	}
 
-    public function setType($type)
-    {
-        $this->type = $type;
+	public function setType($type)
+	{
+		$this->type = $type;
+		return $this;
+	}
 
-        return $this;
-    }
+	public function setPrice($price)
+	{
+		$this->price = $price;
+		return $this;
+	}
 
-    public function setCategory($category)
-    {
-        $this->category = $category;
+	public function setCategory($category)
+	{
+		$this->category = $category;
+		return $this;
+	}
 
-        return $this;
-    }
+	public function setCreatedAt($created_at)
+	{
+		$this->created_at = $created_at;
+		return $this;
+	}
 
-    public function setPrice($price)
-    {
-        $this->price = $price;
+	public function setStatus($status)
+	{
+		$this->status = $status;
+		return $this;
+	}
 
-        return $this;
-    }
+	public function setDescription($description)
+	{
+		$this->description = $description;
+		return $this;
+	}
 
-    public function setCreatedAt($created_at)
-    {
-        $this->created_at = $created_at;
+	public function setPointsEarned($points_earned)
+	{
+		$this->points_earned = $points_earned;
+		return $this;
+	}
 
-        return $this;
-    }
+	public function setApprovedAt($approved_at)
+	{
+		$this->approved_at = $approved_at;
+		return $this;
+	}
 
-    public function setStatus($status)
-    {
-        $this->status = $status;
+	public function setDeniedAt($denied_at)
+	{
+		$this->denied_at = $denied_at;
+		return $this;
+	}
 
-        return $this;
-    }
+	public function setLanguage($language)
+	{
+		$this->language = $language;
+		return $this;
+	}
 
     // Getters
     public function getId()
-    {
-        return $this->id;
-    }
+	{
+		return $this->id;
+	}
 
-    public function getTitle()
-    {
-        return $this->title;
-    }
+	public function getTitle()
+	{
+		return $this->title;
+	}
 
-    public function getProfessor()
-    {
-        return $this->professor;
-    }
+	public function getProfessorName()
+	{
+		return $this->professor_name;
+	}
 
-    public function getType()
-    {
-        return $this->type;
-    }
+	public function getProfessorEmail()
+	{
+		return $this->professor_email;
+	}
 
-    public function getCategory()
-    {
-        return $this->category;
-    }
+	public function getProfessorClassification()
+	{
+		return $this->professor_classification;
+	}
 
-    public function getPrice()
-    {
-        return $this->price;
-    }
+	public function getProfessorCreatedAt()
+	{
+		return $this->professor_created_at;
+	}
 
-    public function getCreatedAt()
-    {
-        return $this->created_at;
-    }
+	public function getType()
+	{
+		return $this->type;
+	}
 
-    public function getStatus()
-    {
-        return $this->status;
-    }
+	public function getPrice()
+	{
+		return $this->price;
+	}
+
+	public function getCategory()
+	{
+		return $this->category;
+	}
+
+	public function getCreatedAt()
+	{
+		return $this->created_at;
+	}
+
+	public function getStatus()
+	{
+		return $this->status;
+	}
+
+	public function getDescription()
+	{
+		return $this->description;
+	}
+
+	public function getPointsEarned()
+	{
+		return $this->points_earned;
+	}
+
+	public function getApprovedAt()
+	{
+		return $this->approved_at;
+	}
+
+	public function getDeniedAt()
+	{
+		return $this->denied_at;
+	}
+
+	public function getLanguage()
+	{
+		return $this->language;
+	}
 
     public function getProperties()
     {
@@ -133,13 +228,22 @@ class CourseApplicationData
 
         $courseData->setId($courseApplication->id);
         $courseData->setTitle($courseApplication->title);
-        $courseData->setProfessor($courseApplication->professor->fullname);
         $courseData->setType($courseApplication->courseType->name);
         $courseData->setCategory($courseApplication->courseCategory->name);
+        $courseData->setLanguage($courseApplication->language);
+        $courseData->setDescription($courseApplication->description);
+
+        $courseData->setProfessorName($courseApplication->professor->fullname);
+        $courseData->setProfessorEmail($courseApplication->professor->email);
+        $courseData->setProfessorClassification($courseApplication->professor->classification->name);
+        $courseData->setProfessorCreatedAt(Carbon::parse($courseApplication->professor->created_at)->format('Y-m-d'));
 
         $courseData->setPrice($courseApplication->price == 0 ? 'Free' : number_format($courseApplication->price, 2));
+        $courseData->setPointsEarned($courseApplication->points_earned == 0 ? "N/A" : number_format($courseApplication->points_earned, 2));
 
         $courseData->setCreatedAt(Carbon::parse($courseApplication->created_at)->format('Y-m-d'));
+        $courseData->setDeniedAt(@$courseApplication->denied_at ? Carbon::parse($courseApplication->denied_at)->format('Y-m-d') : NULL);
+        $courseData->setApprovedAt(@$courseApplication->approved_at ? Carbon::parse($courseApplication->approved_at)->format('Y-m-d') : NULL);
 
         $courseStatus = CourseApplication::PENDING;
 
