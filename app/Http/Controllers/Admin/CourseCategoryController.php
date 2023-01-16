1e0800eb9dfb96c8b5b471a3183f039fdb102d5c
Phillip Quipse <p.quipse@bizwind.co.jp>
2023-01-16 15:47:19 +0800

    Fetch, render and filter records

--- app/Http/Controllers/Admin/CourseCategoryController.php
+++ app/Http/Controllers/Admin/CourseCategoryController.php
@@ -3,15 +3,28 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\CourseCategoryRepository;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class CourseCategoryController extends Controller
 {
-    public function index()
+    private $courseCategoryRepository;
+
+    public function __construct()
     {
-        return Inertia::render('Admin/Settings/CourseCategory')->withViewData([
-            'title' => 'Categories | Admin'
+        $this->courseCategoryRepository = new CourseCategoryRepository();
+    }
+
+    public function index(Request $request)
+    {
+        return Inertia::render('Admin/Settings/CourseCategory', [
+            'categories' => $this->courseCategoryRepository->get($request->all()),
+            'keyword'    => @$request['keyword'] ?? '',
+            'sort'       => @$request['sort'] ?? 'created_at:desc',
+            'page'       => @$request['page'] ?? 1
+        ])->withViewData([
+            'title' => 'Categories | Admin',
         ]);
     }
 }
--- resources/js/Pages/Admin/Settings/CourseCategory.jsx
+++ resources/js/Pages/Admin/Settings/CourseCategory.jsx
@@ -1,9 +1,22 @@
-import { Add, Delete, Edit } from "@mui/icons-material"
-import { Box, Button, Card, CardContent, Grid, IconButton, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material"
+import { useForm, usePage } from "@inertiajs/inertia-react"
+import { Add } from "@mui/icons-material"
+import { Box, Button, Card, CardContent, Grid, Pagination, Stack, Typography } from "@mui/material"
 import Input from "../../../components/forms/Input"
 import { displaySelectOptions } from "../../../helpers/form.helper"
+import CourseCategoryTable from "./components/CourseCategoryTable"
+import TableLoader from "../../../components/common/TableLoader"
+import { handleOnChange, handleOnSelectChange } from "../../../helpers/form.helper"
+import routes from "../../../helpers/routes.helper"
 
 const CourseCategory = () => {
+
+    const { categories, keyword, sort, page } = usePage().props
+
+    const { data: filters, setData: setFilters, get, transform, processing } = useForm({
+        keyword,
+        sort,
+        page
+    })
 
     const sortOptions = [
         { name: 'Name A-Z', value: 'name:asc' },
@@ -11,6 +24,21 @@ const CourseCategory = () => {
         { name: 'Date - Oldest', value: 'created_at:asc' },
         { name: 'Date - Newest', value: 'created_at:desc' }
     ]
+
+    const handleFilterSubmit = e => {
+        e.preventDefault()
+
+        get(routes["admin.settings.categories.index"])
+    }
+
+    const handleOnPaginate = (e, page) => {
+        transform(data => ({
+            ...data,
+            page
+        }))
+
+        handleFilterSubmit(e)
+    }
 
     return (
         <Box>
@@ -29,59 +57,55 @@ const CourseCategory = () => {
             </Stack>
             <Card sx={{ mb: 2 }}>
                 <CardContent>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} md={8}>
-                            <Input
-                                label="Keyword"
-                                placeholder="Search for name"
-                            />
+                    <form onSubmit={handleFilterSubmit}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} md={8}>
+                                <Input
+                                    label="Keyword"
+                                    placeholder="Search for name"
+                                    name="keyword"
+                                    value={filters.keyword}
+                                    onChange={e => handleOnChange(e, setFilters)}
+                                />
+                            </Grid>
+                            <Grid item xs={12} md={3}>
+                                <Input
+                                    label="Sort"
+                                    select
+                                    name="sort"
+                                    value={filters.sort}
+                                    InputLabelProps={{
+                                        shrink: true
+                                    }}
+                                    onChange={e => handleOnSelectChange(e, filters, transform, handleFilterSubmit)}
+                                >
+                                    {displaySelectOptions(sortOptions, 'value')}
+                                </Input>
+                            </Grid>
+                            <Grid item xs={12} md={1}>
+                                <Button
+                                    children="Filter"
+                                    variant="contained"
+                                    fullWidth
+                                />
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} md={3}>
-                            <Input
-                                label="Sort"
-                                select
-                                InputLabelProps={{
-                                    shrink: true
-                                }}
-                            >
-                                {displaySelectOptions(sortOptions, 'value')}
-                            </Input>
-                        </Grid>
-                        <Grid item xs={12} md={1}>
-                            <Button
-                                children="Filter"
-                                variant="contained"
-                                fullWidth
-                            />
-                        </Grid>
-                    </Grid>
+                    </form>
                 </CardContent>
             </Card>
-            <TableContainer component={Paper}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell width="80%" children="Name" />
-                            <TableCell children="Actions" align="center" />
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        <TableRow>
-                            <TableCell children="Lorem Ipsum" />
-                            <TableCell>
-                                <Stack spacing={1} direction="row" justifyContent="center">
-                                    <IconButton size="small" title="Edit">
-                                        <Edit fontSize="inherit" />
-                                    </IconButton>
-                                    <IconButton size="small" title="Delete">
-                                        <Delete color="error" fontSize="inherit" />
-                                    </IconButton>
-                                </Stack>
-                            </TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </TableContainer>
+            {
+                processing
+                ? <TableLoader />
+                : <CourseCategoryTable data={categories.data} />
+            }
+            <Box sx={{ display: 'flex', justifyContent: 'center', my: 4 }}>
+                <Pagination
+                    onChange={handleOnPaginate}
+                    count={categories.last_page}
+                    page={categories.current_page}
+                    color="primary"
+                />
+            </Box>
         </Box>
     )
 }
