ac79f27670b2b60077698b3f7c01096fb1fb1e15
Phillip Quipse <p.quipse@bizwind.co.jp>
2023-01-17 14:27:16 +0800

    Create FormDialog component

---
+++ resources/js/components/common/FormDialog.jsx
@@ -0,0 +1,36 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material"
+
+const FormDialog = ({ open, title, processing, handleClose, handleSubmit, confirmButtonText = 'Submit', cancelButtonText='Cancel', children }) => {
+    return (
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            fullWidth
+            maxWidth="xs"
+            keepMounted
+        >
+            <form onSubmit={handleSubmit}>
+                <DialogTitle children={title} />
+                <DialogContent>
+                    {children}
+                </DialogContent>
+                <DialogActions sx={{ p: 2 }}>
+                    <Button
+                        children={cancelButtonText}
+                        onClick={handleClose}
+                        disabled={processing}
+                    />
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        disabled={processing}
+                        children={confirmButtonText}
+                        onClick={handleSubmit}
+                    />
+                </DialogActions>
+            </form>
+        </Dialog>
+    )
+}
+
+export default FormDialog
