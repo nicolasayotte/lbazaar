58c4998f93231c2248443ea9edad3f1c64260099
Phillip Quipse <p.quipse@bizwind.co.jp>
2023-01-18 15:47:21 +0800

    Layout admin class types page

--- resources/js/Pages/Admin/Settings/CourseTypes/Index.jsx
+++ resources/js/Pages/Admin/Settings/CourseTypes/Index.jsx
@@ -1,6 +1,64 @@
+import { usePage } from "@inertiajs/inertia-react"
+import { Add } from "@mui/icons-material"
+import { Box, Button, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material"
+import Input from "../../../../components/forms/Input"
+
 const Index = () => {
+
+    const { title } = usePage().props
+
     return (
-        <>Course Types</>
+        <Box>
+            <Stack direction="row" justifyContent="space-between" mb={2}>
+                <Typography
+                    variant="h4"
+                    children={title}
+                />
+                <Button
+                    children="Save Changes"
+                    variant="contained"
+                />
+            </Stack>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell children="Class Type" />
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>
+                                <Input
+                                    placeholder="Class Type"
+                                />
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>
+                                <Input
+                                    placeholder="Class Type"
+                                />
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>
+                                <Input
+                                    placeholder="Class Type"
+                                />
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>
+                                <Input
+                                    placeholder="Class Type"
+                                />
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Box>
     )
 }
 
