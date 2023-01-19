8790b6429ec6021e59fb75b6dd0c1384eee89cdb
Phillip Quipse <p.quipse@bizwind.co.jp>
2023-01-19 15:25:26 +0800

    Create UserEducationSeeder and append it to DatabaseSeeder

---
+++ database/seeders/UserEducationSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use App\Models\UserEducation;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Database\Seeder;
+
+class UserEducationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $teacherUsers = User::whereRoleIs(Role::TEACHER)->get();
+
+        foreach ($teacherUsers as $user) {
+            UserEducation::factory()
+                         ->count(fake()->numberBetween(1, 3))
+                         ->state(new Sequence([
+                            'user_id' => $user->id
+                         ]))
+                         ->create();
+        }
+    }
+}
