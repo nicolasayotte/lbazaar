49d3702160903349e0610f7f1bc17eae6fe8461e
Phillip Quipse <p.quipse@bizwind.co.jp>
2023-01-30 11:31:24 +0800

    Add all translatable texts from admin dashboard to lang/en/translatables.php

--- lang/en/translatables.php
+++ lang/en/translatables.php
@@ -75,5 +75,115 @@ return [
         'translations' => [
             'update' => 'Are you sure you want to update these translations?'
         ]
+    ],
+    'title' => [
+        'class' => [
+            'applications' => 'Class Applications',
+            'types' => 'Class Types'
+        ],
+        'users' => 'Manage Users',
+        'inquiries' => 'Inquiries',
+        'categories' => 'Categories',
+        'classifications' => 'Classifications',
+        'profile' => [
+            'edit' => 'Edit Profile'
+        ],
+        'password' => [
+            'update' => 'Update Password'
+        ],
+        'login' => 'Login',
+        'translations' => 'Translations',
+    ],
+    'filters' => [
+        'date' => [
+            'asc' => 'Date - Oldest',
+            'desc' => 'Date - Newest'
+        ],
+        'name' => [
+            'asc' => 'Name A-Z',
+            'desc' => 'Name Z-A'
+        ],
+        'price' => [
+            'asc' => 'Price - Low to High',
+            'desc' => 'Price - High to Low'
+        ],
+        'status' => [
+            'active' => 'Active',
+            'approved' => 'Approved',
+            'denied' => 'Denied',
+            'disabled' => 'Disabled',
+            'pending' => 'Pending'
+        ],
+        'title' => [
+            'asc' => 'Title A-Z',
+            'desc' => 'Title Z-A',
+        ],
+    ],
+    'texts' => [
+        'actions' => 'Actions',
+        'admin' => 'Admin',
+        'all' => 'All',
+        'back_to_top' => 'Back to Top Page',
+        'cancel' => 'Cancel',
+        'category' => 'Category',
+        'class_type' => 'Class Type',
+        'classification' => 'Classification',
+        'commission_rate' => 'Commission Rate',
+        'confirm_password' => 'Confirm Password',
+        'content_information' => 'Content Information',
+        'country' => 'Country',
+        'create_category' => 'Create Category',
+        'create_classification' => 'Create Classification',
+        'current_password' => 'Current Password',
+        'date' => 'Date',
+        'date_applied' => 'Date Applied',
+        'date_approved' => 'Date Approved',
+        'date_created' => 'Date Created',
+        'date_denied' => 'Date Denied',
+        'date_joined' => 'Date Joined',
+        'delete' => 'Delete',
+        'delete_category' => 'Delete Category',
+        'delete_classification' => 'Delete Classification',
+        'edit' => 'Edit',
+        'edit_category' => 'Edit Category',
+        'edit_classification' => 'Edit Classification',
+        'email' => 'Email',
+        'filter' => 'Filter',
+        'first_name' => 'First Name',
+        'general_information' => 'General Information',
+        'keyword' => 'Keyword',
+        'language' => 'Language',
+        'last_name' => 'Last Name',
+        'message' => 'Message',
+        'name' => 'Name',
+        'new_password' => 'New Password',
+        'no_records_found' => 'No Records Found',
+        'points_earned' => 'Points Earned',
+        'price' => 'Price',
+        'pricing_information' => 'Pricing Information',
+        'profile' => 'Profile',
+        'role' => 'Role',
+        'save_changes' => 'Save Changes',
+        'search_name' => 'Search for name',
+        'search_name_email' => 'Search for name or email',
+        'search_name_email_subject' => 'Search for name, email, or subject',
+        'search_title_teacher' => 'Search for title or teacher',
+        'send_reply' => 'Send Reply',
+        'sign_in' => 'Sign In',
+        'sign_out' => 'Sign Out',
+        'sort' => 'Sort',
+        'status' => 'Status',
+        'status_information' => 'Status Information',
+        'subject' => 'Subject',
+        'submit' => 'Submit',
+        'teacher' => 'Teacher',
+        'teacher_information' => 'Teacher Information',
+        'title' => 'Title',
+        'type' => 'Type',
+        'update_password' => 'Update Password',
+        'update_password_help' => 'Enter your current password for verification',
+        'update_password_notice' => 'You will be signed out when your password is updated',
+        'update_profile' => 'Update Profile',
+        'view' => 'View',
     ]
 ];
