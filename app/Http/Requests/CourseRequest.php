ca09df4f9cd79cdb803002cae0d3b5722c5b0731
Phillip Quipse <p.quipse@bizwind.co.jp>
2023-02-28 17:50:03 +0800

    Add form request class for creating courses

---
+++ app/Http/Requests/CourseRequest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Course;
+use App\Models\Role;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CourseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->user() && auth()->user()->hasRole(Role::TEACHER);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'description' => 'required',
+            'course_category_id' => 'required',
+            'course_type_id' => 'required',
+            'max_participant' => 'integer|required|min:1',
+            'image_thumbnail' => 'required|file|image|dimensions:min_width=800,min_height=600',
+            'price' => 'numeric|required_if:course_type_id,1',
+            'points_earned' => 'numeric|required_if:course_type_id,3',
+            'format' => 'required',
+            'zoom_link' => 'required_if:format,' . Course::LIVE,
+            'video_path' => 'required_if:format,' . Course::ON_DEMAND,
+            'is_cancellable' => 'boolean',
+            'days_before_cancellation' => 'required_if:is_cancellable,true'
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        $attributes = [
+            'course_category_id' => getTranslation('texts.category'),
+            'course_type_id' => getTranslation('texts.type'),
+            'image_thumbnail' => getTranslation('texts.class_image'),
+            'zoom_link' => getTranslation('texts.zoom_link'),
+            'video_path' => getTranslation('texts.video'),
+            'is_cancellable' => getTranslation('texts.cancellable'),
+            'days_before_cancellation' => getTranslation('texts.days')
+        ];
+
+        return array_map('strtolower', $attributes);
+    }
+}
