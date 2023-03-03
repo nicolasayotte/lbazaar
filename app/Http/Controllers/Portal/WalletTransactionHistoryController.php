f6481e8bc59daad8f72e2a1be0822d4c7e623031
Aaron Jet Sarreal <a.sarreal@bizwind.co.jp>
2023-03-03 14:56:26 +0800

    localization

--- app/Http/Controllers/Portal/WalletTransactionHistoryController.php
+++ app/Http/Controllers/Portal/WalletTransactionHistoryController.php
@@ -3,23 +3,13 @@
 namespace App\Http\Controllers\Portal;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\SearchClassRequest;
-use App\Models\Country;
-use App\Models\WalletTransactionHistory;
-use App\Repositories\CourseCategoryRepository;
-use App\Repositories\CourseHistoryRepository;
-use App\Repositories\CourseRepository;
-use App\Repositories\CourseTypeRepository;
-use App\Repositories\UserRepository;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class WalletTransactionHistoryController extends Controller
 {
     public function index(Request $request)
     {
-        // dd(@$request['page']);
         $walletTransactionHistory = auth()->user()->userWallet()->first()->userWalletTransactions()->paginate(10);
         return Inertia::render('Portal/MyPage/WalletHistory/Index', [
             'page'           => @$request['page'] ?? 1,
