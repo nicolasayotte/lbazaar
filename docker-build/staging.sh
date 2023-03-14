745cace64a551d932627f0268fae9c322ee5a76f
Aaron Jet Sarreal <a.sarreal@bizwind.co.jp>
2023-03-14 13:16:11 +0800

    fix staging deployment

--- docker-build/staging.sh
+++ docker-build/staging.sh
@@ -4,7 +4,7 @@ docker build --rm -t lebazaar-app -t lebazaar-app-staging:latest -f Dockerfile.s
 docker compose -f docker-compose.staging.yml rm -sf app # Stop only the app container
 docker compose -f docker-compose.staging.yml -p lebazaar-app --env-file /var/.secrets/.env.staging up -d
 sleep 10
-docker compose -f docker-compose.staging.yml -p lebazaar-app exec app php artisan optimize:clear
+# docker compose -f docker-compose.staging.yml -p lebazaar-app exec app php artisan optimize:clear
 docker compose -f docker-compose.staging.yml -p lebazaar-app exec app php artisan migrate --force
 docker compose -f docker-compose.staging.yml -p lebazaar-app exec app php artisan route:cache
 docker compose -f docker-compose.staging.yml -p lebazaar-app exec app php artisan view:cache
